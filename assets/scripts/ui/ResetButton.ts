e45c32382e1a9c168cfddd12d06422f9bb7b70b0
DESKTOP-RR6M8F2\PC <tientien187@gmail.com>
2026-07-28 10:09:57 +0700

    Update data

--- assets/scripts/ui/ResetButton.ts
+++ assets/scripts/ui/ResetButton.ts
@@ -1,11 +1,12 @@
 import { _decorator, Component, Button } from 'cc';
+import { GameManager } from '../managers/GameManager';
 import { LevelManager } from '../managers/LevelManager';
 
 const { ccclass, property } = _decorator;
 
 /**
  * ResetButton - Attach vào một Button node trong scene.
- * Khi nhấn sẽ trigger level failed (restart màn hiện tại).
+ * Khi nhấn sẽ reset ngay màn hiện tại.
  */
 @ccclass('ResetButton')
 export class ResetButton extends Component {
@@ -13,12 +14,14 @@ export class ResetButton extends Component {
         const button = this.getComponent(Button);
         if (button) {
             button.node.on('click', this.onResetClicked, this);
-        } else {
-                    }
+        }
     }
 
     private onResetClicked(): void {
-                LevelManager.getInstance().onLevelFailed();
+        const levelId = LevelManager.getInstance().getCurrentLevelId();
+        if (levelId > 0) {
+            GameManager.Instance?.startLevel(levelId);
+        }
     }
 
     protected onDestroy(): void {
